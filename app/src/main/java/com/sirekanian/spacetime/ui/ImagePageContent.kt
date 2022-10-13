9e76a9dae390c188be700d2682b619059829d198
sirekanian <sirekanian@pm.me>
2022-10-13 22:31:11 +0400

    added text button intead of icon

--- app/src/main/java/com/sirekanian/spacetime/ui/ImagePageContent.kt
+++ app/src/main/java/com/sirekanian/spacetime/ui/ImagePageContent.kt
@@ -1,13 +1,9 @@
 package com.sirekanian.spacetime.ui
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Slider
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.*
@@ -64,7 +60,11 @@ fun ImagePageContent(
             Row {
                 VectorIconButton(Icons.Default.ArrowBack, onClick = { isEditMode = false })
                 Spacer(Modifier.weight(1f))
-                VectorIconButton(Icons.Default.Delete, onClick = { onDelete() })
+                if (name.isBlank()) {
+                    TextButton(onClick = { onDelete() }) {
+                        Text("DELETE", color = MaterialTheme.colors.error)
+                    }
+                }
                 VectorIconButton(Icons.Default.Done, onClick = {
                     onDone(ImagePage(page.id, name, page.url, page.date, blur))
                     isEditMode = false
