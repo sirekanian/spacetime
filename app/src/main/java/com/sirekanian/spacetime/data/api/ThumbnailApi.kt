5611c15448f8fb6c79d9532e5ea609dc4ffafc41
Vadik Sirekanyan <me@sirekanyan.org>
2026-01-15 21:15:51 +0100

    Changed media url type to nullable

--- app/src/main/java/com/sirekanian/spacetime/data/api/ThumbnailApi.kt
+++ app/src/main/java/com/sirekanian/spacetime/data/api/ThumbnailApi.kt
@@ -2,11 +2,11 @@ package com.sirekanian.spacetime.data.api
 
 import com.sirekanian.spacetime.data.api.model.NasaMedia
 import com.sirekanian.spacetime.model.Thumbnail
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.request.*
-import io.ktor.serialization.kotlinx.json.*
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.datetime.LocalDate
 import kotlinx.serialization.json.Json
 
@@ -24,8 +24,15 @@ class ThumbnailApi {
     suspend fun getThumbnails(date: LocalDate): List<Thumbnail> =
         httpClient.get("https://sirekanian.com/apod/json/$date.json")
             .body<List<NasaMedia>>()
-            .filter { it.media_type == "image" && it.url.startsWith(baseImageUrl) }
-            .map { Thumbnail(baseThumbnailUrl + it.url.removePrefix(baseImageUrl), date) }
+            .filter { it.mediaType == "image" }
+            .mapNotNull { media ->
+                if (media.url?.startsWith(baseImageUrl) == true) {
+                    val imagePath = media.url.removePrefix(baseImageUrl)
+                    Thumbnail(baseThumbnailUrl + imagePath, date)
+                } else {
+                    null
+                }
+            }
             .reversed()
 
 }
--- app/src/main/java/com/sirekanian/spacetime/data/api/model/NasaMedia.kt
+++ app/src/main/java/com/sirekanian/spacetime/data/api/model/NasaMedia.kt
@@ -1,9 +1,10 @@
 package com.sirekanian.spacetime.data.api.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class NasaMedia(
-    val url: String,
-    val media_type: String,
+    @SerialName("url") val url: String? = null,
+    @SerialName("media_type") val mediaType: String,
 )
